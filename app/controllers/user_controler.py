2cf91cc73df40eb9d77c5c7c7a008bec78e73687
Guilhermino Lucas <guilherminolucasc@gmail.com>
2022-02-01 15:00:00 -0300

    Feat: adding the get users route logic

--- app/controllers/user_controler.py
+++ app/controllers/user_controler.py
@@ -3,4 +3,11 @@ from http import HTTPStatus
 from app.models.user_model import User
 
 def get_all():
-    ...
+    User.check_database()
+    all_users = User.get_all()
+    if not all_users:
+        User.add_data()
+    user_keys = ['id','name','age','eyeColor','company','email','profilePic']
+    all_users = [dict(zip(user_keys,user))for user in all_users]
+    return jsonify(all_users)
+    
