88a699c4fa2dea0495d8b198e70652252b5947ae
Guilhermino Lucas <guilherminolucasc@gmail.com>
2022-02-03 19:50:07 -0300

    feat: adding CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from app import routes
+from flask_cors import CORS
 
 def create_app():
     app = Flask(__name__)
 
     app.config["JSON_SORT_KEYS"] = False
     routes.init_app(app)
+    CORS(app)
     return app
